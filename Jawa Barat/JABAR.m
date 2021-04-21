ef5d4e37789802866560610dcd7ce5490bf91c69
amartyanatayu <82956528+amartyanatayu@users.noreply.github.com>
2021-04-21 21:57:51 +0700

    Add files via upload

---
+++ Jawa Barat/JABAR.m
@@ -0,0 +1,57 @@
+%Average Mean Temperature Jabar Island 2019 - 2020
+
+TJabarJan19 = mean(JabarJanuari2019(:,1),'omitnan');
+TJabarFeb19 = mean(JabarFebruari2019(:,1),'omitnan');
+TJabarMar19 = mean(JabarMaret2019(:,1),'omitnan');
+TJabarApr19 = mean(JabarApril2019(:,1),'omitnan');
+TJabarMay19 = mean(JabarMei2019(:,1),'omitnan');
+TJabarJun19 = mean(JabarJuni2019(:,1),'omitnan');
+TJabarJul19 = mean(JabarJuli2019(:,1),'omitnan');
+TJabarAgt19 = mean(JabarAgustus2019(:,1),'omitnan');
+TJabarSep19 = mean(JabarSeptember2019(:,1),'omitnan');
+TJabarOct19 = mean(JabarOktober2019(:,1),'omitnan');
+TJabarNov19 = mean(JabarNovember2019(:,1),'omitnan');
+TJabarDec19 = mean(JabarDesember2019(:,1),'omitnan');
+TJabarJan20 = mean(JabarJanuari2020(:,1),'omitnan');
+TJabarFeb20 = mean(JabarFebruari2020(:,1),'omitnan');
+TJabarMar20 = mean(JabarMaret2020(:,1),'omitnan');
+TJabarApr20 = mean(JabarApril2020(:,1),'omitnan');
+TJabarMay20 = mean(JabarMei2020(:,1),'omitnan');
+TJabarJun20 = mean(JabarJuni2020(:,1),'omitnan');
+TJabarJul20 = mean(JabarJuli2020(:,1),'omitnan');
+TJabarAgt20 = mean(JabarAgustus2020(:,1),'omitnan');
+TJabarSep20 = mean(JabarSeptember2020(:,1),'omitnan');
+TJabarOct20 = mean(JabarOktober2020(:,1),'omitnan');
+TJabarNov20 = mean(JabarNovember2020(:,1),'omitnan');
+TJabarDec20 = mean(JabarDesember2020(:,1),'omitnan');
+TJabar = [TJabarJan19; TJabarFeb19; TJabarMar19; TJabarApr19; TJabarMay19; TJabarJun19; TJabarJul19; TJabarAgt19; TJabarSep19; TJabarOct19; TJabarNov19; TJabarDec19; TJabarJan20; TJabarFeb20; TJabarMar20; TJabarApr20; TJabarMay20; TJabarJun20; TJabarJul20; TJabarAgt20; TJabarSep20; TJabarOct20; TJabarNov20; TJabarDec20];
+
+
+% Average Rainfall Jabar Island 2019 - 2020
+
+RJabarJan19 = mean(JabarJanuari2019(:,2),'omitnan');
+RJabarFeb19 = mean(JabarFebruari2019(:,2),'omitnan');
+RJabarMar19 = mean(JabarMaret2019(:,2),'omitnan');
+RJabarApr19 = mean(JabarApril2019(:,2),'omitnan');
+RJabarMay19 = mean(JabarMei2019(:,2),'omitnan');
+RJabarJun19 = mean(JabarJuni2019(:,2),'omitnan');
+RJabarJul19 = mean(JabarJuli2019(:,2),'omitnan');
+RJabarAgt19 = mean(JabarAgustus2019(:,2),'omitnan');
+RJabarSep19 = mean(JabarSeptember2019(:,2),'omitnan');
+RJabarOct19 = mean(JabarOktober2019(:,2),'omitnan');
+RJabarNov19 = mean(JabarNovember2019(:,2),'omitnan');
+RJabarDec19 = mean(JabarDesember2019(:,2),'omitnan');
+RJabarJan20 = mean(JabarJanuari2020(:,2),'omitnan');
+RJabarFeb20 = mean(JabarFebruari2020(:,2),'omitnan');
+RJabarMar20 = mean(JabarMaret2020(:,2),'omitnan');
+RJabarApr20 = mean(JabarApril2020(:,2),'omitnan');
+RJabarMay20 = mean(JabarMei2020(:,2),'omitnan');
+RJabarJun20 = mean(JabarJuni2020(:,2),'omitnan');
+RJabarJul20 = mean(JabarJuli2020(:,2),'omitnan');
+RJabarAgt20 = mean(JabarAgustus2020(:,2),'omitnan');
+RJabarSep20 = mean(JabarSeptember2020(:,2),'omitnan');
+RJabarOct20 = mean(JabarOktober2020(:,2),'omitnan');
+RJabarNov20 = mean(JabarNovember2020(:,2),'omitnan');
+RJabarDec20 = mean(JabarDesember2020(:,2),'omitnan');
+RJabar = [RJabarJan19; RJabarFeb19; RJabarMar19; RJabarApr19; RJabarMay19; RJabarJun19; RJabarJul19; RJabarAgt19; RJabarSep19; RJabarOct19; RJabarNov19; RJabarDec19; RJabarJan20; RJabarFeb20; RJabarMar20; RJabarApr20; RJabarMay20; RJabarJun20; RJabarJul20; RJabarAgt20; RJabarSep20; RJabarOct20; RJabarNov20; RJabarDec20];
+
